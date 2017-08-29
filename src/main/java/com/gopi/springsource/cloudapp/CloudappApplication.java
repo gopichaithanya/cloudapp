36c3f4c61a582216ef37f718604fbe2cb3066d62
Thirunagari Gopichaithanya <gopichaithanya@users.noreply.github.com>
2017-08-29 05:41:10 +0530

    Initial commit

---
+++ src/main/java/com/gopi/springsource/cloudapp/CloudappApplication.java
@@ -0,0 +1,12 @@
+package com.gopi.springsource.cloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudappApplication.class, args);
+	}
+}
